3bdfc1f6dc1929a2b6a948cc570666987bc1d56f
Kenneth <dao.khoi@1bytesoftware.com>
2024-11-04 20:06:17 +0700

    Add Select variant

--- src/molecules/Select/Select.tsx
+++ src/molecules/Select/Select.tsx
@@ -3,7 +3,16 @@ import ConditionalWrapper from '../../atomics/ConditionalWrapper';
 import { ConfigProviderDesign } from '../../ConfigProviderDesign';
 import { LabelField } from '../LabelField';
 import { SelectWrapper } from './styles';
-import { ISelectProps } from './types';
+import { ISelectProps, variantSelectExtend } from './types';
+import useExtendVariant from './useExtendVariant';
+import { IRdComponentsConfig } from '../../organisms';
+import { config } from '../..';
+
+const isVariantSelectExtend = (
+    variant: NonNullable<ISelectProps['variant']>
+): variant is variantSelectExtend => {
+    return ['outlined-transparent'].includes(variant);
+};
 
 export const Select = ({
     label: labelFieldProps,
@@ -12,9 +21,16 @@ export const Select = ({
     width,
     minWidth,
     isHideValueOnLoading = true,
+    variant,
 
     ...antdProps
 }: ISelectProps) => {
+    let newSelectDesignToken: IRdComponentsConfig['Select'] = {
+        ...config.componentToken?.Select,
+        algorithm: true,
+    };
+
+    //#region  Handle isHideValueOnLoading prop
     // Handle hide value when loading
     if (isHideValueOnLoading) {
         antdProps = {
@@ -22,16 +38,39 @@ export const Select = ({
             value: !antdProps.loading && antdProps.value,
         };
     }
+    //#endregion
+
+    if (variant && isVariantSelectExtend(variant)) {
+        // Get design token config for color.
+        const designTokenConfig = useExtendVariant(variant);
+
+        // Merge design token config with button design token.
+        newSelectDesignToken = {
+            ...newSelectDesignToken,
+            ...designTokenConfig,
+        };
+
+        variant = 'outlined';
+    }
+
+    //#region
+    // Handle extend variant props
+
+    //#endregion
 
     return (
-        <ConfigProviderDesign>
+        <ConfigProviderDesign
+            componentToken={{
+                Select: newSelectDesignToken,
+            }}
+        >
             <SelectWrapper className="rc-select__wrapper" width={width} minWidth={minWidth}>
                 <ConditionalWrapper
                     condition={Boolean(labelFieldProps)}
                     wrapper={LabelField}
                     wrapperProps={labelFieldProps}
                 >
-                    <SelectAntd {...(antdProps as SelectProps)} />
+                    <SelectAntd variant={variant} {...(antdProps as SelectProps)} />
                 </ConditionalWrapper>
             </SelectWrapper>
         </ConfigProviderDesign>
