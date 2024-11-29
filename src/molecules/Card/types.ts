c773ddb7fa473165967338e28eb111b9b878fbf7
Kenneth <dao.khoi@1bytesoftware.com>
2024-11-29 17:48:58 +0700

    feature: add compact variant for Card molecule

--- src/molecules/Card/types.ts
+++ src/molecules/Card/types.ts
@@ -1,5 +1,16 @@
 import { CardProps } from 'antd';
 
-type RdCardPropsExtend = {};
+type RdCardPropsExtend = {
+    /**
+     * @description
+     * Defines the visual style of the entire card component.
+     *
+     * - `"default"`: The card will use the standard styling provided by Ant Design.
+     * - `"compact"`: Applies a specialized style to the card, including a narrower header and a layout resembling compact designs such as Google Forms.
+     *
+     * This property allows you to customize the appearance of the card to match specific design requirements.
+     */
+    variant?: 'default' | 'compact';
+};
 
 export type RdCardProps = CardProps & RdCardPropsExtend;
