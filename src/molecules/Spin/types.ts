9d8f0fb51c0c2fd1b0edf0e8dc47c1f9eff567cd
Kenneth <dao.khoi@1bytesoftware.com>
2024-12-10 10:25:22 +0700

    feature: add custom component token for Spin molecule

--- src/molecules/Spin/types.ts
+++ src/molecules/Spin/types.ts
@@ -1,4 +1,10 @@
 import { SpinProps } from 'antd';
+import { ComponentToken } from 'antd/es/spin/style';
+
+/**
+ * @description Override SpinComponentToken of antd.
+ */
+export type SpinComponentToken = ComponentToken & {};
 
 export interface RdSpinProps extends SpinProps {
     
