b9d04245ee8e0ec42ff596c0ef4c363de273552c
Kenneth <dao.khoi@1bytesoftware.com>
2024-12-10 10:21:08 +0700

    fix: variant style prop for button molecule

--- src/molecules/Card/Card.tsx
+++ src/molecules/Card/Card.tsx
@@ -1,11 +1,17 @@
+import clsx from 'clsx';
 import { ConfigProviderDesign } from '../../ConfigProviderDesign';
 import { CardStyles } from './styles';
 import { RdCardProps } from './types';
 
-export const Card = ({ ...antdProps }: RdCardProps) => {
+export const Card = ({ className, variant = 'default', ...antdProps }: RdCardProps) => {
+    const variantClass: Record<NonNullable<RdCardProps['variant']>, string> = {
+        compact: 'rd-card-variant-compact',
+        default: '',
+    };
+
     return (
         <ConfigProviderDesign>
-            <CardStyles {...antdProps} />
+            <CardStyles className={clsx(className, variantClass[variant])} {...antdProps} />
         </ConfigProviderDesign>
     );
 };
