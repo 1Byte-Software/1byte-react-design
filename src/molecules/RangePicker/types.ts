aef3ad6400e914a40803f9297f3eca5becceb7e9
Kenneth <dao.khoi@1bytesoftware.com>
2024-12-10 10:24:01 +0700

    feature: add custom component token for RangePicker molecule

--- src/molecules/RangePicker/types.ts
+++ src/molecules/RangePicker/types.ts
@@ -1,6 +1,12 @@
 import { RangePickerProps } from 'antd/es/date-picker';
 import { IRegistryRangePickerControlField } from '../../models';
 import { RdLabelFieldWrapperProps } from '../LabelField/types';
+import { ComponentToken } from 'antd/es/date-picker/style';
+
+/**
+ * @description Override DatePickerComponentToken of antd.
+ */
+export type DatePickerComponentToken = ComponentToken & {};
 
 export interface RdRangePickerProps extends RangePickerProps, RdLabelFieldWrapperProps {}
 
