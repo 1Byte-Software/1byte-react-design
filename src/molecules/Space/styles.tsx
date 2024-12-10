4e90647e2d884e85820610c67107c2a0670a2803
Kenneth <dao.khoi@1bytesoftware.com>
2024-12-10 10:24:59 +0700

    feature: remove forward prop block for Space molecule

--- src/molecules/Space/styles.tsx
+++ src/molecules/Space/styles.tsx
@@ -2,6 +2,15 @@ import styled from '@emotion/styled';
 import { Space } from 'antd';
 import { RdSpaceProps } from './types';
 
-export const SpaceStyles = styled(Space)<Pick<RdSpaceProps, 'block'>>`
-    ${({ block }) => block && 'width: 100%'};
+export const SpaceStyles = styled(Space, {
+    shouldForwardProp: prop => {
+        const propsNotForward = ['block'];
+
+        // If prop is not in the propsNotForward array, then forward it.
+        return !propsNotForward.includes(prop);
+    },
+})<Pick<RdSpaceProps, 'block'>>`
+    ${({ block }) => {
+        return block && 'width: 100%';
+    }};
 `;
