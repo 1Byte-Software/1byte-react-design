dc583ed3ffbf099188d13e19528901a004eee94b
Kenneth <dao.khoi@1bytesoftware.com>
2024-12-10 10:24:32 +0700

    feature: add forward ref and fix type name for Select molecule

--- src/molecules/Select/Select.tsx
+++ src/molecules/Select/Select.tsx
@@ -1,13 +1,13 @@
 import { Select as SelectAntd, SelectProps } from 'antd';
+import { forwardRef, LegacyRef, RefAttributes } from 'react';
+import { config } from '../..';
 import ConditionalWrapper from '../../atomics/ConditionalWrapper';
 import { ConfigProviderDesign } from '../../ConfigProviderDesign';
+import { RdComponentsConfig } from '../../organisms';
 import { LabelField } from '../LabelField';
 import { SelectWrapper } from './styles';
-import { RdSelectProps, variantSelectExtend } from './types';
+import { RdSelectProps, RdSelectRef, variantSelectExtend } from './types';
 import useExtendVariant from './useExtendVariant';
-import { IRdComponentsConfig } from '../../organisms';
-import { config } from '../..';
-import { RefAttributes } from 'react';
 
 const isVariantSelectExtend = (
     variant: NonNullable<RdSelectProps['variant']>
@@ -15,66 +15,67 @@ const isVariantSelectExtend = (
     return ['outlined-transparent'].includes(variant);
 };
 
-export const Select = ({
-    label: labelFieldProps,
+export const Select = forwardRef(
+    (
+        props: RdSelectProps & RefAttributes<RdSelectProps>,
+        ref: LegacyRef<RdSelectRef> | undefined
+    ) => {
+        let {
+            label: labelFieldProps,
 
-    // Custom props
-    width,
-    minWidth,
-    isHideValueOnLoading = true,
-    variant,
-    ref,
+            // Custom props
+            width,
+            minWidth,
+            isHideValueOnLoading = true,
+            variant,
 
-    ...antdProps
-}: RdSelectProps & RefAttributes<RdSelectProps>) => {
-    let newSelectDesignToken: IRdComponentsConfig['Select'] = {
-        ...config.componentToken?.Select,
-        algorithm: true,
-    };
+            ...antdProps
+        } = props;
 
-    //#region  Handle isHideValueOnLoading prop
-    // Handle hide value when loading
-    if (isHideValueOnLoading) {
-        antdProps = {
-            ...antdProps,
-            value: !antdProps.loading && antdProps.value,
-        };
-    }
-    //#endregion
-
-    if (variant && isVariantSelectExtend(variant)) {
-        // Get design token config for color.
-        const designTokenConfig = useExtendVariant(variant);
-
-        // Merge design token config with button design token.
-        newSelectDesignToken = {
-            ...newSelectDesignToken,
-            ...designTokenConfig,
+        let newSelectDesignToken: RdComponentsConfig['Select'] = {
+            ...config.componentToken?.Select,
+            algorithm: true,
         };
 
-        variant = 'outlined';
+        //#region  Handle isHideValueOnLoading prop
+        // Handle hide value when loading
+        if (isHideValueOnLoading) {
+            antdProps = {
+                ...antdProps,
+                value: !antdProps.loading && antdProps.value,
+            };
+        }
+        //#endregion
+
+        if (variant && isVariantSelectExtend(variant)) {
+            // Get design token config for color.
+            const designTokenConfig = useExtendVariant(variant);
+
+            // Merge design token config with button design token.
+            newSelectDesignToken = {
+                ...newSelectDesignToken,
+                ...designTokenConfig,
+            };
+
+            variant = 'outlined';
+        }
+
+        return (
+            <ConfigProviderDesign
+                componentToken={{
+                    Select: newSelectDesignToken,
+                }}
+            >
+                <SelectWrapper className="rc-select__wrapper" width={width} minWidth={minWidth}>
+                    <ConditionalWrapper
+                        condition={Boolean(labelFieldProps)}
+                        wrapper={LabelField}
+                        wrapperProps={labelFieldProps}
+                    >
+                        <SelectAntd ref={ref} variant={variant} {...(antdProps as SelectProps)} />
+                    </ConditionalWrapper>
+                </SelectWrapper>
+            </ConfigProviderDesign>
+        );
     }
-
-    //#region
-    // Handle extend variant props
-
-    //#endregion
-
-    return (
-        <ConfigProviderDesign
-            componentToken={{
-                Select: newSelectDesignToken,
-            }}
-        >
-            <SelectWrapper className="rc-select__wrapper" width={width} minWidth={minWidth}>
-                <ConditionalWrapper
-                    condition={Boolean(labelFieldProps)}
-                    wrapper={LabelField}
-                    wrapperProps={labelFieldProps}
-                >
-                    <SelectAntd variant={variant} {...(antdProps as SelectProps)} />
-                </ConditionalWrapper>
-            </SelectWrapper>
-        </ConfigProviderDesign>
-    );
-};
+);
