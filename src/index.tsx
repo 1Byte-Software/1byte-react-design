66b50a0a35eb2e0616d2ef73ca77795d6c4fad3b
Kenneth <dao.khoi@1bytesoftware.com>
2024-12-15 18:44:41 +0700

    feature: remove export models

--- src/index.tsx
+++ src/index.tsx
@@ -14,8 +14,5 @@ export const config: IConfig = {
 };
 
 export * from './atomics';
-export * from './models';
 export * from './molecules';
 export * from './organisms';
-
-export const version = '0.0.6';
