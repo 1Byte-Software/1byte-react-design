213d9f29b171c0500e702cdc667ed4b0b9c306c9
Kenneth <dao.khoi@1bytesoftware.com>
2024-12-15 18:33:01 +0700

    refactor: Modal molecule typescript

--- src/molecules/Modal/types.ts
+++ src/molecules/Modal/types.ts
@@ -1,9 +1,20 @@
-import { ModalProps } from 'antd';
-import { ComponentToken } from 'antd/es/modal/style';
+import { Modal, GetProps } from 'antd';
+import { ComponentToken as ModalComponentTokenAntd } from 'antd/es/modal/style';
 
-/**
- * @description Override ModalComponentToken of antd.
- */
-export type ModalComponentToken = ComponentToken & {};
+//#region Define props
+type ModalProps = GetProps<typeof Modal>;
+//#endregion
 
-export interface RdModalProps extends ModalProps {}
+//#region Custom component token
+type ModalComponentTokenExtend = {};
+//#endregion
+
+//#region Custom props
+type ModalPropsExtend = {};
+//#endregion
+
+//#region export type
+export type RdModalProps = ModalProps & ModalPropsExtend;
+
+export type RdModalComponentToken = ModalComponentTokenAntd & ModalComponentTokenExtend;
+//#endregion
