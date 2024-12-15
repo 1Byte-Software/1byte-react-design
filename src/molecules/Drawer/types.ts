c9274201080880dd3c2efd1c29ddf5ef024cf238
Kenneth <dao.khoi@1bytesoftware.com>
2024-12-15 18:33:52 +0700

    refactor: Drawer molecule typescript

--- src/molecules/Drawer/types.ts
+++ src/molecules/Drawer/types.ts
@@ -1,9 +1,19 @@
-import { DrawerProps } from 'antd';
-import { ComponentToken } from 'antd/es/drawer/style';
+import { Drawer, GetProps } from 'antd';
+import { ComponentToken as DrawerComponentTokenAntd } from 'antd/es/drawer/style';
 
-/**
- * @description Override DrawerComponentToken of antd.
- */
-export type DrawerComponentToken = ComponentToken & {};
+//#region Define props
+type DrawerProps = GetProps<typeof Drawer>;
+//#endregion
 
-export interface RdDrawerProps extends DrawerProps {}
+//#region Custom component token
+type DrawerComponentTokenExtend = {};
+//#endregion
+
+//#region Custom props
+type DrawerPropsExtend = {};
+//#endregion
+
+//#region export type
+export type RdDrawerProps = DrawerProps & DrawerPropsExtend;
+export type RdDrawerComponentToken = DrawerComponentTokenAntd & DrawerComponentTokenExtend;
+//#endregion
