5031c530ea32009794f991138500178e0a642f8b
Kenneth <dao.khoi@1bytesoftware.com>
2024-12-15 18:32:33 +0700

    refactor: Collapse molecule typescript

--- src/molecules/Collapse/types.ts
+++ src/molecules/Collapse/types.ts
@@ -1,9 +1,20 @@
-import { CollapseProps } from 'antd';
-import { ComponentToken } from 'antd/es/collapse/style';
+import { Collapse, GetProps } from 'antd';
+import { ComponentToken as CollapseComponentTokenAntd } from 'antd/es/collapse/style';
 
-/**
- * @description Override CollapseComponentToken of antd.
- */
-export type CollapseComponentToken = ComponentToken & {};
+//#region Define props
+type CollapseProps = GetProps<typeof Collapse>;
+//#endregion
 
-export interface RdCollapseProps extends CollapseProps {}
+//#region Custom component token
+type CollapseComponentTokenExtend = {};
+//#endregion
+
+//#region Custom props
+type CollapsePropsExtend = {};
+//#endregion
+
+//#region export type
+export type RdCollapseProps = CollapseProps & CollapsePropsExtend;
+
+export type RdCollapseComponentToken = CollapseComponentTokenAntd & CollapseComponentTokenExtend;
+//#endregion
