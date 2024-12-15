492703d6ed014ee18e910f831d34892a56ecc140
Kenneth <dao.khoi@1bytesoftware.com>
2024-12-15 18:43:02 +0700

    feature: remove getAxisStyles util

--- src/utils/styles.tsx
+++ src/utils/styles.tsx
@@ -1,19 +1,3 @@
-import { css } from '@emotion/react';
-import { TAxis } from '../models';
-
-export const getAxisStyles = (axis?: TAxis) => {
-    if (axis === 'vertical') {
-        return css`
-            display: flex;
-            flex-direction: column;
-        `;
-    }
-    return css`
-        display: flex;
-        flex-direction: row;
-    `;
-};
-
 export const getExcludeForwardProps = <T extends any>(
     propsNotForward: (keyof T)[],
     prop: string
