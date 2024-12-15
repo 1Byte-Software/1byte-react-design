694d552ccdf3ce158457a3c8b11e0fe792fdd489
Kenneth <dao.khoi@1bytesoftware.com>
2024-12-15 18:40:11 +0700

    refactor: Space molecule typescript

--- src/molecules/Space/types.ts
+++ src/molecules/Space/types.ts
@@ -1,14 +1,29 @@
-import { SpaceProps } from 'antd';
-import { ComponentToken } from 'antd/es/space/style';
+import { Space, GetProps } from 'antd';
+import { ComponentToken as SpaceComponentTokenAntd } from 'antd/es/space/style';
 
-/**
- * @description Override SpaceComponentToken of antd.
- */
-export type SpaceComponentToken = ComponentToken & {};
+//#region Define props
+type SpaceProps = GetProps<typeof Space>;
+type CompactProps = GetProps<typeof Space.Compact>;
+//#endregion
 
-export type RdSpaceProps = SpaceProps & {
+//#region Custom component token
+type SpaceComponentTokenExtend = {};
+//#endregion
+
+//#region Custom props
+type SpacePropsExtend = {
     /**
      * @description Option to fit Space width to its parent width, default is false
      */
     block?: boolean;
 };
+
+type CompactPropsExtend = {};
+//#endregion
+
+//#region export type
+export type RdSpaceProps = SpaceProps & SpacePropsExtend;
+export type RdCompactProps = CompactProps & CompactPropsExtend;
+
+export type RdSpaceComponentToken = SpaceComponentTokenAntd & SpaceComponentTokenExtend;
+//#endregion
