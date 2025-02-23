bd58d572698f8b2e8702134e7309c0e484592947
Kenneth <dao.khoi@1bytesoftware.com>
2025-02-23 20:40:12 +0700

    feature: add Popconfirm molecule

---
+++ src/molecules/Popconfirm/Popconfirm.tsx
@@ -0,0 +1,6 @@
+import { PopconfirmStyles } from './styles';
+import { RdPopconfirmComponent } from './types';
+
+export const Popconfirm: RdPopconfirmComponent = props => {
+    return <PopconfirmStyles {...props} />;
+};
---
+++ src/molecules/Popconfirm/styles.tsx
@@ -0,0 +1,4 @@
+import styled from '@emotion/styled';
+import { Popconfirm } from 'antd';
+
+export const PopconfirmStyles = styled(Popconfirm)``;
