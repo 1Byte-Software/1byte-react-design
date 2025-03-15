2c686a4131e8d9677a3a17c4771411ae1fb10a49
Kenneth <dao.khoi@1bytesoftware.com>
2025-03-15 22:11:12 +0700

    fix: Fix Header ref

--- src/molecules/Layout/Header.tsx
+++ src/molecules/Layout/Header.tsx
@@ -2,6 +2,6 @@ import { forwardRef } from 'react';
 import { HeaderStyles } from './styles';
 import { RdLayoutHeaderComponent } from './types';
 
-export const LayoutHeader: RdLayoutHeaderComponent = forwardRef(props => {
-    return <HeaderStyles {...props} />;
+export const LayoutHeader: RdLayoutHeaderComponent = forwardRef((props, ref) => {
+    return <HeaderStyles ref={ref} {...props} />;
 });
