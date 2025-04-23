e996951e82e0a5e361b38891580ab5089f85a838
Kenneth <dao.khoi@1bytesoftware.com>
2025-04-23 21:59:27 +0700

    #feature: Add loading for Image molecule

--- src/molecules/Image/styles.tsx
+++ src/molecules/Image/styles.tsx
@@ -1,4 +1,18 @@
 import styled from '@emotion/styled';
 import { Image } from 'antd';
+import { getExcludeForwardProps } from '../../utils';
+import { RdImageProps } from './types';
 
-export const ImageStyles = styled(Image)``;
+export const ImageStyles = styled(Image, {
+    shouldForwardProp: prop =>
+        getExcludeForwardProps<RdImageProps>([] as (keyof RdImageProps)[], prop),
+})<RdImageProps>`
+    ${({ hidden }) => {
+        return (
+            hidden &&
+            `
+                display: none
+            `
+        );
+    }}
+`;
--- src/molecules/Image/types.ts
+++ src/molecules/Image/types.ts
@@ -10,11 +10,15 @@ type ImageComponentTokenExtend = {};
 //#endregion
 
 //#region Define extended types
-type ImagePropsExtend = {};
+type ImagePropsExtend = {
+    // Override loading prop in antd image, use loadingType instead
+    loading?: boolean;
+    loadingType?: ImagePropsAntd['loading'];
+};
 //#endregion
 
 //#region Export types
-export type RdImageProps = ImagePropsAntd & ImagePropsExtend;
+export type RdImageProps = Omit<ImagePropsAntd, 'loading'> & ImagePropsExtend;
 export type RdImageComponentToken = ImageComponentTokenAntd & ImageComponentTokenExtend;
 //#endregion
 
