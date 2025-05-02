d2f266626da84f591e632a25a84211ae9709574d
Kenneth <dao.khoi@1bytesoftware.com>
2025-05-02 09:37:47 +0700

    #feature: add disableMargin prop to Divider molecule

--- src/molecules/Divider/styles.tsx
+++ src/molecules/Divider/styles.tsx
@@ -1,4 +1,16 @@
 import styled from '@emotion/styled';
 import { Divider } from 'antd';
+import { getExcludeForwardProps } from '../../utils';
+import { RdDividerProps } from './types';
+import { css } from '@emotion/react';
 
-export const DividerStyles = styled(Divider)``;
+export const DividerStyles = styled(Divider, {
+    shouldForwardProp: prop =>
+        getExcludeForwardProps<RdDividerProps>(['disableMargin'] as (keyof RdDividerProps)[], prop),
+})<RdDividerProps>`
+    ${({ disableMargin }) => {
+        return disableMargin && css`
+            margin: 0;
+        `;
+    }}
+`;
