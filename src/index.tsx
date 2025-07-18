3c64bb2d1fdbc35fc192ce23766d124fb4b17b28
Kenneth <dao.khoi@1bytesoftware.com>
2025-07-18 14:08:10 +0700

    fix: fix export berrel

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
-import { theme } from 'antd';
-import { RdThemeConfig } from './organisms';
+export * from '@ant-design/icons';
 
 export * from './config';
-export * from './utils';
 export * from './types';
+export * from './utils';
 
 export * from './atomics';
 export * from './molecules';
