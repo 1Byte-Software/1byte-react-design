221b85d1cf0d853599c665f7a944a37cfeae69ff
Kenneth <dao.khoi@1bytesoftware.com>
2025-09-18 18:18:14 +0700

    feat: add chart to summary card

--- src/organisms/SummaryCard/types.ts
+++ src/organisms/SummaryCard/types.ts
@@ -1,7 +1,26 @@
 import { ReactNode } from 'react';
 import { RdCardProps } from '../../molecules';
+import { StatisticProps } from 'antd';
+import { ChartDataItem } from './TotalLineChart/types';
 
-export interface RdSummaryCardProps extends RdCardProps {
+export interface RdSummaryCardProps extends Omit<RdCardProps, 'title'> {
+    title: StatisticProps['title'];
+
+    /**
+     * The main numeric value to display.
+     * Usually represents the total amount or main KPI.
+     */
+    value: StatisticProps['value'];
+
+    trend?: {
+        isPositive?: boolean;
+        changePercent: number;
+    };
+
+    statistic?: Omit<StatisticProps, 'title' | 'value'>;
+
+    icon?: StatisticProps['prefix'];
+
     /**
      * Determines the display format of values.
      *
@@ -12,11 +31,10 @@ export interface RdSummaryCardProps extends RdCardProps {
      */
     format?: 'number' | 'currency';
 
-    /**
-     * The main numeric value to display.
-     * Usually represents the total amount or main KPI.
-     */
-    value: ReactNode;
+    chart?: {
+        data: ChartDataItem[];
+        color?: string;
+    };
 
     /**
      * The secondary numeric value to display.
