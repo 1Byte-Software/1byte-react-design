586707573ca8f114c64aaf0c966b5382dd1b05a1
Kenneth <dao.khoi@1bytesoftware.com>
2025-10-06 15:05:29 +0700

    JFW-873 feat: add Tag.Group component

--- src/molecules/Tag/Tag.tsx
+++ src/molecules/Tag/Tag.tsx
@@ -2,17 +2,20 @@ import { forwardRef } from 'react';
 import { Skeleton } from '../Skeleton';
 import { TagStyled } from './styles';
 import { TagCheckable } from './TagCheckable';
+import { TagGroup } from './TagGroup';
 import { RdTagComponent, RdTagCompoundedComponent } from './types';
+import clsx from 'clsx';
 
 export const TagInternal: RdTagComponent = forwardRef((props, ref) => {
-    const { loading = false, ...antdProps } = props;
+    const { loading = false, type = 'default', className, ...antdProps } = props;
 
     if (loading) {
         return <Skeleton.Button active />;
     }
 
-    return <TagStyled ref={ref} {...antdProps} />;
+    return <TagStyled ref={ref} type={type} className={clsx("rd-tag", className)} {...antdProps} />;
 });
 
 export const Tag = TagInternal as RdTagCompoundedComponent;
 Tag.CheckableTag = TagCheckable;
+Tag.Group = TagGroup;
